c9997e56d200966e51c4e41e70312e52ceed533b
derPascalD <pascal_daniel@gmx.de>
2024-02-20 14:32:19 +0100

    Add Option Menu and SearchLobby Menu

--- public/mainMenu/mainMenu.js
+++ public/mainMenu/mainMenu.js
@@ -1,15 +1,61 @@
 
-
-
-window.onload = function(){
+// Set Username after successful login
+window.onload = function () {
     document.getElementById("username").textContent = getCookieValue('username');
 }
 
-
-function logOut(){
-    socket.emit("logout", null);
+function logOut() {
+    socket.emit("logout");
 }
 
 socket.on('reloadPage', () => {
     window.location.reload();
 });
+
+btn1;
+btn2;
+
+function openOptions() {
+
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("option").style.display = "flex";
+
+    btn2 = document.createElement("button");
+    let t = document.createTextNode("Zurück");
+    btn2.appendChild(t);
+    btn2.className = "btn-select";
+    document.getElementById("option").appendChild(btn2);
+    btn2.setAttribute("onclick", "changeButtonText()");
+
+}
+
+
+function changeButtonText(){
+
+    if (btn2) btn2.remove();
+    document.getElementById("menu").style.display = "flex";
+    document.getElementById("option").style.display = "none";
+}
+
+
+
+function searchLobby() {
+    document.getElementById("searchLobby").style.display = "flex";
+    document.getElementById("menu").style.display = "none";
+
+    btn1 = document.createElement("button");
+    let t = document.createTextNode("Test");
+    btn1.appendChild(t);
+    btn1.className = "btn-select";
+    document.getElementById("searchLobby").appendChild(btn1);
+
+}
+
+
+
+function backMenu() {
+    if (btn1) btn1.remove();
+    document.getElementById("menu").style.display = "flex";
+    document.getElementById("searchLobby").style.display = "none";
+}
+
